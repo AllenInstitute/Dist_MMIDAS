e467039be4c47f5738ecc012885723d0f065040b
Hilal Mufti <hmufti@cs.washington.edu>
2025-06-03 02:06:01 -0700

    enhance: remove redundant code from _utils

--- mmidas/_utils.py
+++ mmidas/_utils.py
@@ -14,6 +14,3 @@ def set_seeds(s: int) -> None:
     np.random.seed(s)
     random.seed(s)
     os.environ["PYTHONHASHSEED"] = str(s)
-
-def params_is_equal(ps1: dict[str, th.Tensor], ps2: dict[str, th.Tensor]) -> bool:
-    return set(ps1.keys()) == set(ps2.keys()) and all(th.equal(ps1[k], ps2[k]) for k in ps1)
